57f7d9d06b0f39c4299f78f752ad7cead883496e
Igor Faustino <igornfaustino@gmail.com>
2019-12-10 01:29:55 +0000

    add media query

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,40 +1,93 @@
 import { useState, useEffect } from 'react';
 
+function getWindowDimensions() {
+	const { innerWidth: width, innerHeight: height } = window;
+	return {
+		width,
+		height
+	};
+}
+
+export const useWindowDimensions = () => {
+	const [windowDimensions, setWindowDimensions] = useState(
+		getWindowDimensions()
+	);
+
+	useEffect(() => {
+		function handleResize() {
+			setWindowDimensions(getWindowDimensions());
+		}
+
+		window.addEventListener('resize', handleResize);
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
+
+	return windowDimensions;
+};
+
 export const useFocusInput = input => {
-	const [width, setWidth] = useState('100%');
-	const [heigth, setHeigth] = useState('100%');
+	const [width, setWidth] = useState('0');
+	const [heigth, setHeigth] = useState('0');
 	const [position, setPosition] = useState({});
+	const { width: winWidth } = useWindowDimensions();
+
+	console.log(winWidth);
 
 	useEffect(() => {
 		switch (input) {
 			case 'number':
+				if (winWidth < 405) {
+					setHeigth('30px');
+					setPosition({
+						top: '42%',
+						left: '5%'
+					});
+				} else {
+					setHeigth('40px');
+					setPosition({
+						top: '44%',
+						left: '5%'
+					});
+				}
 				setWidth('90%');
-				setHeigth('40px');
-				setPosition({
-					top: '44%',
-					left: '5%'
-				});
 				break;
 			case 'fullname':
-				setWidth('77%');
-				setHeigth('40px');
-				setPosition({
-					top: '77%',
-					left: '2%'
-				});
+				if (winWidth < 405) {
+					setHeigth('30px');
+					setWidth('73%');
+					setPosition({
+						top: '75%',
+						left: '2%'
+					});
+				} else {
+					setHeigth('50px');
+					setWidth('77%');
+					setPosition({
+						top: '77%',
+						left: '2%'
+					});
+				}
 				break;
 			case 'date':
+				if (winWidth < 405) {
+					setHeigth('40px');
+					setPosition({
+						top: '74%',
+						left: '78%'
+					});
+				} else {
+					setHeigth('50px');
+					setPosition({
+						top: '77%',
+						left: '80%'
+					});
+				}
 				setWidth('17%');
-				setHeigth('50px');
-				setPosition({
-					top: '77%',
-					left: '80%'
-				});
 				break;
 			default:
 				break;
 		}
-	}, [input]);
+	}, [input, winWidth]);
 
 	return [width, heigth, position];
 };
